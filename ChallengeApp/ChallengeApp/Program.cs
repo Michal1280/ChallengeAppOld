10b3378f730c4af21e91f6414cb30b21eefda755
Michal <michal.zebrowski.it@gmail.com>
2023-04-13 10:14:52 +0200

    Another request commit

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Tom and Jerry");
 Console.WriteLine("Ja good");
+Console.WriteLine("New Commit");
