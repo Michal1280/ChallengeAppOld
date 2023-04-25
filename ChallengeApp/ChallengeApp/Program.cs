1461a9e6a724ed556c5307a1b0e128d6383a3b30
michalzebrowski <Michal.zebrowski@external.danone.com>
2023-04-25 23:46:38 +0200

    Day 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,32 +1,83 @@
-﻿//Napisz w Program.cs kod, w ktrórym: 
-//    -zadeklarujesz zmienną z imieniam, 
-//    -zaeklarujesz zmienną z płacią (dobierz zmienną),
-//    -zaeklarujesz zmienną z wiekiem,
-//    -zwerifikujesz dane i wyślietlisz
-//    // jesten z kominikatówL 
-//    (1): "Kobieta poniżej 30 lat"
-//    (2): "Ewa, lat 33"
-//    (3): "Niepełnoletni Mężczyzna"
+﻿//Prepare programm which ile count number of digites which appeares in variable. 
 
-//    (Możesz dodać swoje)
+//    For example:
+//    Number: 4566
+//    0 => 0
+//    1 => 0
+//    2 => 0
+//    3 => 0
+//    4 => 1
+//    5 => 1
+//    6 => 2
+//    7 => 0
+//    8 => 0
+//    9 => 0
 
-var name = "Michał";
-var age = 15;
-char gender = 'm';
+int number = 4566;
+string numberToString = number.ToString();
+char[] letters = numberToString.ToArray();
 
-if ((gender == 'k') && (age < 30))
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char num in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (num == '0')
+    {
+        counter0++;
+    }else if (num == '1')
+    {
+        counter1++;
+    }
+    else if (num == '2')
+    {
+        counter2++;
+    }
+    else if (num == '3')
+    {
+        counter3++;
+    }
+    else if (num == '4')
+    {
+        counter4++;
+    }
+    else if (num == '5')
+    {
+        counter5++;
+    }
+    else if (num == '6')
+    {
+        counter6++;
+    }
+    else if (num == '7')
+    {
+        counter7++;
+    }
+    else if (num == '8')
+    {
+        counter8++;
+    }
+    else if (num == '9')
+    {
+        counter9++;
+    }
 }
-else if ((name == "Ewa") && (age == 33))
-{
-    Console.WriteLine($"{name}, lat {age}");
-}
-else if ((age < 18) && (gender == 'm'))
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else
-{
-    Console.WriteLine("Osoba nie jest niepełnoletnim mężczyzną, ani Ewą 33 lata, ani kobietą poniżej 30 roku życia.");
-}
+Console.WriteLine($"0-> {counter0}");
+Console.WriteLine($"1-> {counter1}");
+Console.WriteLine($"2-> {counter2}");
+Console.WriteLine($"3-> {counter3}");
+Console.WriteLine($"4-> {counter4}");
+Console.WriteLine($"5-> {counter5}");
+Console.WriteLine($"6-> {counter6}");
+Console.WriteLine($"7-> {counter7}");
+Console.WriteLine($"8-> {counter8}");
+Console.WriteLine($"9-> {counter9}");
+
